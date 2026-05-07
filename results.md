caef7fc24bea89783ced87981b4c687b158060e0
maxb <maxbergl@kth.se>
2026-05-07 09:05:46 +0200

    Add lam-search and more details to results.md

--- results.md
+++ results.md
@@ -1,9 +1,28 @@
 # E-Level
 
-## Basic model
-Model with no extra implementations as a baseline.
+## Results
 
 | Model | Lambda (λ) | Performance | f | n_f | Cycles | Epochs | m | k | Batch Size | Etas |
 | :--- | :--- | :--- | :--- | :--- | :--- | :--- | :--- | :--- | :--- | :--- |
-| **Model 1** | 0.003 | 66.91% | 4 | 40 | 4 | 4 | 400 | 10 | 100 | [1e-5, 0.1] |
-| **Model 2** | 0.005 | 66.18% | 4 | 40 | 4 | 4 | 400 | 10 | 100 | [1e-5, 0.1] |
+| **v.0** | 0.003 | 66.91% | 4 | 40 | 4 | 4 | 400 | 10 | 100 | [1e-5, 0.1] |
+| **v.0** | 0.005 | 66.18% | 4 | 40 | 4 | 4 | 400 | 10 | 100 | [1e-5, 0.1] |
+| **v.1** | ... | ...% | 4 | 40 | 3 | 4 | 400 | 10 | 100 | [1e-5, 0.1] |
+
+## Models
+
+# v.0
+Model with no extra implementations as a baseline. 
+SGD optimizer, cyclic learning rate and lambda regularization, 
+No data augmentation, no dropout, no normalization and no label smoothening.
+
+# v.1
+Added Adam Optimizer and VGG block.
+
+# Todo
+Add batch normalization.
+
+Add data augmentation.
+
+Add dropout.
+
+Add label smoothening.
--- src2/main.py
+++ src2/main.py
@@ -20,13 +20,32 @@ def trainNet():
     res = network.evaluate()
     print(res)
 
+def lambdaSearch(lams):
+    CN_params = {'f': 4, 'n_f': 40, 'n_s': 800}
+    GD_params = {'n_cycles': 3, 'n_epochs': 3, 'n_hidden': 300, 'k': 10, 'n_batch': 100}
+    LR_params = {'etas': [0.00001, 0.1]}
+    results = {}
+    
+    for lam in lams:
+        print(f"Testing lambda: {lam}")
+        GD_params['lam'] = lam
+        network = Network(LR_params, GD_params, CN_params)
+        network.trainModel()
+        res = network.evaluate()
+        results[lam] = res
+    
+    print("Lambda Search Results:")
+    for lam, res in results.items():
+        print(f"Lambda: {lam}, Accuracy: {res}")
+    print("Best Lambda:", max(results, key=lambda k: results[k]))
+
 def argParser():
     parser = argparse.ArgumentParser(description="CNN Project Runner")
     parser.add_argument(
         "--mode", 
         type=str, 
         default="train", 
-        help="Mode to run the script in: 'full', 'train', or 'test'"
+        help="Mode to run the script in: 'full', 'lam-search', 'train', or 'test'"
     )
 
     return parser.parse_args()
@@ -38,6 +57,8 @@ def main():
     
     if args.mode == "full":
         trainNet()
+    elif args.mode == "lam-search":
+        lambdaSearch([0.0001, 0.00025, 0.0005, 0.001]) # Lambdas = [0.0001, 0.00025, 0.0005, 0.001, 0.0025, 0.005, 0.01]
     else:
         print(f"Mode '{args.mode}' is not implemented yet.")
     
