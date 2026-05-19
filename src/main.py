2f8df1eb798e28f9203dafa6c191a83a99bacc02
Hex4C <58385827+Hex4C@users.noreply.github.com>
2026-05-19 16:10:54 +0200

    chore: Format code

--- src/main.py
+++ src/main.py
@@ -1,49 +1,71 @@
 import os
 import warnings
+
 import numpy as np
-from network import Network
+
 from mainHelpers import argParser, lambdaSearch
+from network import Network
 
 os.environ["PYTHONWARNINGS"] = "ignore"
 warnings.simplefilter("ignore")
 
+
 def trainNet():
-    CN_params = {'f': 4, 'n_f': 40, 'n_s': 800}
-    GD_params = {'n_cycles': 1, 'n_epochs': 60, 'n_hidden': 300, 'k': 10, 'n_batch': 100, 'img_size': 32, 'lam': 0.0001}
+    CN_params = {"f": 4, "n_f": 40, "n_s": 800}
+    GD_params = {
+        "n_cycles": 1,
+        "n_epochs": 60,
+        "n_hidden": 300,
+        "k": 10,
+        "n_batch": 100,
+        "img_size": 32,
+        "lam": 0.0001,
+    }
     CN_params = {
-        'l_patchify': {'f': 2, 's': 2, 'n_f': 64},
-        'l_vgg1': {'f': 3, 's': 1, 'n_f': 64},
-        'l_vgg2': {'f': 3, 's': 1, 'n_f': 128},
-        'l_vgg3': {'f': 3, 's': 1, 'n_f': 256},
-        'l_fc2': {'in': 256, 'out': GD_params['k']}
+        "l_patchify": {"f": 2, "s": 2, "n_f": 64},
+        "l_vgg1": {"f": 3, "s": 1, "n_f": 64},
+        "l_vgg2": {"f": 3, "s": 1, "n_f": 128},
+        "l_vgg3": {"f": 3, "s": 1, "n_f": 256},
+        "l_fc2": {"in": 256, "out": GD_params["k"]},
     }
     LR_params = {
-        'eta': 1e-2,
-        'scheduler': True,
-        'scheduler_type': "cosine"} # "step", "cosine", ...
+        "eta": 1e-2,
+        "scheduler": True,
+        "scheduler_type": "cosine",
+    }  # "step", "cosine", ...
     RE_params = {
-        'dropout': True, 'dropout_rates': [0.3, 0.4, 0.5, 0.5], 
-        'augementation': True, 'flip_prob': 0.5, 'shift_max': 0.1,
-        'label_smoothing': True, 'smoothing_factor': 0.1}
-    
+        "dropout": True,
+        "dropout_rates": [0.3, 0.4, 0.5, 0.5],
+        "augementation": True,
+        "flip_prob": 0.5,
+        "shift_max": 0.1,
+        "label_smoothing": True,
+        "smoothing_factor": 0.1,
+    }
+
     network = Network(LR_params, GD_params, CN_params, RE_params)
     network.trainModel(plot=True)
     res = network.evaluate(network.testloader)[0]
-    print(f'Final Test Accuracy: {res}')
-
+    print(f"Final Test Accuracy: {res}")
 
 
 def main():
-    np.random.seed(42) # TODO: Remove for more randomness, which can sometimes give better results
-    
+    np.random.seed(
+        42
+    )  # TODO: Remove for more randomness, which can sometimes give better results
+
     args = argParser()
-    
+
     if args.mode == "full":
         trainNet()
     elif args.mode == "lam-search":
-        lambdaSearch([0.0001, 0.00025, 0.0005, 0.001]) # Lambdas = [0.0001, 0.00025, 0.0005, 0.001, 0.0025, 0.005, 0.01]
+        lambdaSearch(
+            [0.0001, 0.00025, 0.0005, 0.001]
+        )  # Lambdas = [0.0001, 0.00025, 0.0005, 0.001, 0.0025, 0.005, 0.01]
     else:
         print(f"Mode '{args.mode}' is not implemented yet.")
-    
+
+
 if __name__ == "__main__":
     main()
+
--- src/plotHelper.py
+++ src/plotHelper.py
@@ -1,27 +1,29 @@
 import matplotlib.pyplot as plt
 
+
 def plotPerformance(results):
-    loss, val_loss = results['loss'], results['val_loss']
-    acc, val_acc = results['acc'], results['val_acc']
-    x_axis = results['steps']
-    
+    loss, val_loss = results["loss"], results["val_loss"]
+    acc, val_acc = results["acc"], results["val_acc"]
+    x_axis = results["steps"]
+
     plt.figure(figsize=(12, 4))
-    
+
     plt.subplot(1, 2, 1)
-    plt.plot(x_axis, loss, label='Training Loss')
-    plt.plot(x_axis, val_loss, label='Validation Loss')
-    plt.xlabel('Update step', fontsize=14)
-    plt.ylabel('Loss', fontsize=14)
-    plt.title('Loss', fontsize=18)
+    plt.plot(x_axis, loss, label="Training Loss")
+    plt.plot(x_axis, val_loss, label="Validation Loss")
+    plt.xlabel("Update step", fontsize=14)
+    plt.ylabel("Loss", fontsize=14)
+    plt.title("Loss", fontsize=18)
     plt.legend(fontsize=14)
 
     plt.subplot(1, 2, 2)
-    plt.plot(x_axis, acc, label='Training Accuracy')
-    plt.plot(x_axis, val_acc, label='Validation Accuracy')
-    plt.xlabel('Epochs', fontsize=14)
-    plt.ylabel('Accuracy', fontsize=14)
-    plt.title('Accuracy', fontsize=18)
+    plt.plot(x_axis, acc, label="Training Accuracy")
+    plt.plot(x_axis, val_acc, label="Validation Accuracy")
+    plt.xlabel("Epochs", fontsize=14)
+    plt.ylabel("Accuracy", fontsize=14)
+    plt.title("Accuracy", fontsize=18)
     plt.legend(fontsize=14)
 
     plt.tight_layout()
     plt.show()
+
